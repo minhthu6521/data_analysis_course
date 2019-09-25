ce4cdff3c3c039dc9e5e5f03ac7c114198be955d
minhthu6521 <minhthu6521@gmail.com>
2019-09-25 23:37:01 +0300

    template for next exercise

---
+++ diamond.py
@@ -0,0 +1,5 @@
+'''
+Create a function diamond that returns a two dimensional integer array where the 1s form a diamond shape. Rest of the numbers are 0. The function should get a parameter that tells the length of a side of the diamond. Do this using the eye and concatenate functions of NumPy and array slicing.
+'''
+def diamond(len):
+    pass
---
+++ tests/test_diamond.py
@@ -0,0 +1,11 @@
+import pytest
+from diamond import diamond
+from numpy import np
+
+
+@pytest.mark.parametrize('len, expected', [
+    (3, "0 0 1 0 0; 0 1 0 1 0; 1 0 0 0 1; 0 1 0 1 0; 0 0 1 0 0"),
+    (1, [[1]])
+])
+def test_diamond(len, exptected):
+    assert np.array_equal(diamond(len), exptected) is True
